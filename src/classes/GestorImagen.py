db7cba469dcaaaa25a5d04c711e28e888f58bb85
Jose Luis Bautista <joseluis27@ciencias.unam.mx>
2022-11-13 23:12:03 -0600

    Metodo que se encarga de crear las copias

--- src/classes/GestorImagen.py
+++ src/classes/GestorImagen.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy
 from numpy import asarray
 
 class GestorImage:
@@ -6,4 +7,61 @@ class GestorImage:
     def __init__(self, imagen):
         self.imagen = imagen
         img = Image.open(f'{imagen}')
-        self.matrizImagen = asarray(img)
+        self.matrizImagen = asarray(img).tolist()
+        self.almacenadorFiguras = []
+
+    def buscadorColoresImagen(self):
+        coloresImagen = []
+        coloresImagen.append(self.matrizImagen[0][0])
+        for i in range(1,len(self.matrizImagen)):
+            for j in range(1, len(self.matrizImagen[0])):
+                if(self.matrizImagen[i][j] not in coloresImagen):
+                    coloresImagen.append(self.matrizImagen[i][j])
+
+        return coloresImagen            
+    def crearCopia(self):
+        return self.matrizImagen.copy()
+    
+    def separadorFiguras(self):
+        colores = self.buscadorColoresImagen()
+        colorFondo = colores.pop(0)
+        while( len(colores) != 0):
+            imagen = self.crearCopia()
+            colorFigura = colores.pop(0)
+            for i in range(1,len(imagen)):
+                for j in range(1, len(imagen[0])):
+                    if(( imagen[i][j] != colorFondo) and ( imagen[i][j] != colorFigura)):
+                        imagen[i][j] = colorFondo
+            self.almacenadorFiguras.append(imagen)
+            
+    def obtenerMatriz(self):
+        return self.matrizImagen
+            
+    def obtenerImagen(self):
+        return self.imagen
+    
+    def obtenerAlmacenador(self):
+        return self.almacenadorFiguras
+
+def main():
+
+    x = GestorImage('../assets/example_2.bmp')
+    #c2 = x.buscadorColoresImagen()
+    x.separadorFiguras()
+    
+    #figuras = x.obtenerAlmacenador()    
+    #imagen1 = figuras.pop(0)
+    #newNumpy1 = numpy.array(imagen1)
+    #print(newNumpy1)
+    #data1 = Image.fromarray((newNumpy1).astype(numpy.uint8))
+    #data1.save('example_1_01.bmp')
+
+ 
+
+
+    
+    #data = Image.fromarray(m)
+    #data.save('example_1_01.bmp')
+
+
+main()
