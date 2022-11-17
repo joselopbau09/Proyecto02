40e9a760381101d2778aea1d2a1d3f653390f532
eduardo-reyes <eduardoreyes@ciencias.unam.mx>
2022-11-17 00:49:12 -0600

    Correción de gestor y análisis

--- src/classes/AnalisisDatos.py
+++ src/classes/AnalisisDatos.py
@@ -6,27 +6,34 @@ class AnalisisDatos:
     def cambiarArreglo(self, nuevoArreglo):
         self.arregloRayos = nuevoArreglo
 
-    def encuentraMaximos(self, sumas):
-        maximos = 0
-        for i in sumas:
-            if(sumas[i] >= 0 and sumas[i+1] < 0):
-                maximos = maximos + 1
-        return maximos
-
     def analizaArreglo(self):
         sumas = []
+        longArreglo = len(self.arregloRayos)
         print(len(self.arregloRayos))
-        for i in range(len(self.arregloRayos)):
+        for i in range(longArreglo):
+            suma = 0
             for j in range(5):
-                if((i+j+1) < len(self.arregloRayos)-1):
-                    diferencia = self.arregloRayos[i+j+1] - self.arregloRayos[i+j]
-                    sumas.append(diferencia)
+                if((i+j+1) < longArreglo):
+                    suma += self.arregloRayos[i+j+1] - self.arregloRayos[i+j]
+                else:
+                    suma += self.arregloRayos[i+j+1 - longArreglo] - self.arregloRayos[i+j - longArreglo]
+            sumas.append(suma)
+        print(sumas)
         maximos = self.encuentraMaximos(sumas)
+        return maximos
+
+    def encuentraMaximos(self, sumas):
+        maximos = 0
+        for i in range(len(sumas)):
+            if((i+1) < len(sumas)):
+                if((sumas[i] >= 0 and sumas[i+1] < 0) or (sumas[i] > 0 and sumas[i+1] <= 0)):
+                    maximos += 1
         return maximos
 
 
 def main():
-    arreglo = [1,2,3,4,3,2,1]
+    #arreglo = [1,2,3,4,3,2,1]
+    arreglo = [1,2,3,4,3,2,2,3,4,5,6,7,6,5,4,3,3]
     o = AnalisisDatos(arreglo)
     print(o.analizaArreglo())
 
--- src/classes/GestorImagen.py
+++ src/classes/GestorImagen.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import copy
 import numpy
 from numpy import asarray
 import os
@@ -8,35 +9,33 @@ class GestorImage:
     def __init__(self, imagen):
         self.imagen = imagen
         img = Image.open(f'{imagen}')
-        self.matrizImagen = asarray(img).tolist()
+        self.__matrizImagen = asarray(img).tolist()
         self.almacenadorFiguras = []
 
     def buscadorColoresImagen(self):
         coloresImagen = []
-        coloresImagen.append(self.matrizImagen[0][0])
-        for i in range(1,len(self.matrizImagen)):
-            for j in range(1, len(self.matrizImagen[0])):
-                if(self.matrizImagen[i][j] not in coloresImagen):
-                    coloresImagen.append(self.matrizImagen[i][j])
-        return coloresImagen    
+        coloresImagen.append(self.__matrizImagen[0][0])
+        for i in range(1,len(self.__matrizImagen)):
+            for j in range(1, len(self.__matrizImagen[0])):
+                if(self.__matrizImagen[i][j] not in coloresImagen):
+                    coloresImagen.append(self.__matrizImagen[i][j])
 
-    def crearCopia(self):
-        return self.matrizImagen.copy()
+        return coloresImagen            
     
     def separadorFiguras(self):
         colores = self.buscadorColoresImagen()
         colorFondo = colores.pop(0)
         while( len(colores) != 0):
-            imagen = self.crearCopia()
-            colorFigura = colores.pop(0)
+            imagen = copy.deepcopy( self.obtenerMatriz())
+            colorFigura = colores.pop()
             for i in range(1,len(imagen)):
                 for j in range(1, len(imagen[0])):
                     if(( imagen[i][j] != colorFondo) and ( imagen[i][j] != colorFigura)):
                         imagen[i][j] = colorFondo
             self.almacenadorFiguras.append(imagen)
-            
+    
     def obtenerMatriz(self):
-        return self.matrizImagen
+        return self.__matrizImagen
             
     def obtenerImagen(self):
         return self.imagen
@@ -47,17 +46,18 @@ class GestorImage:
 def main():
 
     absolute_folder_path = os.path.dirname(os.path.realpath(__file__))
-    absolute_image_path = os.path.join(absolute_folder_path, '../assets/example_1.bmp')
+    absolute_image_path = os.path.join(absolute_folder_path, '../assets/example_2.bmp')
     x = GestorImage(absolute_image_path)
     #c2 = x.buscadorColoresImagen()
     x.separadorFiguras()
     
-    figuras = x.obtenerAlmacenador()    
+    figuras = x.obtenerAlmacenador()
+    print(len(figuras))    
     imagen1 = figuras.pop(0)
     newNumpy1 = numpy.array(imagen1)
     #print(newNumpy1)
     data1 = Image.fromarray((newNumpy1).astype(numpy.uint8))
-    data1.save('example_1_01.bmp')
+    data1.save('example_2_01.bmp')
 
 
 main()
