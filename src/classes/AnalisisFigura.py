53a8d3e511337d44e808db53bfd34a1d6c98a434
eduardo-reyes <eduardoreyes@ciencias.unam.mx>
2022-11-20 01:04:46 -0600

    Documentación y pruebas de AnalisisFigura

--- src/classes/AnalisisFigura.py
+++ src/classes/AnalisisFigura.py
@@ -1,16 +1,56 @@
 import math
 
+""" Módulo AnalisisFigura """
 class AnalisisFigura:
+    """ Clase que se encarga del análisis de cada figura dentro de la imagen.
+
+    Atributos: 
+        imagenFigura (arreglo2D): Arreglo que contiene la información de cada
+            pixel de la imagen que únicamente contiene una figura con el fondo.
+        colorFondo (tupla): Tupla RGB con el color de fondo
+    Métodos:
+        cambiarImagen(imagenFigura (arreglo2D)):
+            Cambia los atributos de la clase por los de una nueva imagen.
+        buscadorCentro():
+            Busca las coordenadas del centro de la figura en la imagen.
+        calcularHipotenusa():
+            Calcula un radio alrededor de la figura.
+        dentro(coord_x(int),coord_y(int)):
+            Verifica si la coordenada especificada está dentro de la figura.
+        rayos(centro_x(int),centro_y(int),hipotenusa(float)):
+            Crea un arreglo de longitudes de rayos del centro de la figura.
+        obtenerImagen():
+            Getter para la imagen.
+    """
 
     def __init__(self, imagenFigura):
+        """ 
+        Constructor que inicializa los atributos de la clase.
+        """
         self.imagenFigura = imagenFigura
         self.colorFondo = self.imagenFigura[0][0]
 
     def cambiarImagen(self,imagenFigura):
+        """
+        Cambia los atributos de la clase por los de una nueva imagen.
+
+        Parámetros:
+            imagenFigura (arreglo2D): Arreglo de la nueva imagen a analizar.
+        """
         self.imagenFigura = imagenFigura
         self.colorFondo = self.imagenFigura[0][0]
 
     def buscadorCentro(self):
+        """
+        Busca las coordenadas en x e y del centro de la figura en la imagen.
+        Recorre la imagen vertical y horizontalmente sumando las posiciones
+        en x e y si el pixel pertenece a la figura, además de aumentar el
+        conteo del total de pixeles de la figura. Al final saca las coordenadas
+        del centro promediando ambas sumas.
+
+        Regresa:
+        Coordenada x y coordenada y en la imagen del centro de la figura. (int,int)
+        """
         suma_x = 0
         suma_y = 0
         numPixeles = 0
@@ -23,10 +63,18 @@ class AnalisisFigura:
                     numPixeles += 1
         centro_x = suma_x / numPixeles
         centro_y = suma_y / numPixeles
-        #self.setPixel(centro_x, centro_y, [0,0,0])
         return centro_x, centro_y
 
     def calculaHipotenusa(self):
+        """
+        Calcula un radio alrededor de la figura con el fin de aproximar
+        su borde para poder usarlo como hipotenusa en el método
+        trigonométrico de los rayos.
+
+        Regresa:
+        hipotenusa (int): Distancia aproximada que va del centro de la
+        figura al borde más lejano de la misma.
+        """
         min_x = len(self.imagenFigura[0])
         max_x = 0
         min_y = len(self.imagenFigura)
@@ -48,22 +96,45 @@ class AnalisisFigura:
         hipotenusa = max(anchura, altura) / 2
         return hipotenusa
 
-    def setPixel(self, x, y, color):
-        self.imagenFigura[int(y)][int(x)] = color
-
     def dentro(self, coord_x, coord_y):
+        """
+        Determina si el pixel en la coordenada ingresada está dentro
+        de la imagen y pertenece a la figura.
+
+        Parámetros:
+        coord_x (int): Coordenada en x del pixel.
+        coord_y (int): Coordenada en y del pixel.
+
+        Regresa:
+        Valor de verdad que es verdadero si el pixel está dentro
+        de la figura.
+        """
         colorPixel = self.imagenFigura[int(coord_y)][int(coord_x)]
         altura = len(self.imagenFigura)
         anchura = len(self.imagenFigura[0])
         if(coord_x > anchura or coord_y > altura):
             return False
         if(colorPixel != self.colorFondo):
-            self.setPixel(coord_x, coord_y, [0,0,0])
             return True
         else:
             return False
 
     def rayos(self, centro_x, centro_y, hipotenusa):
+        """
+        Método que crea un arreglo de distancias entre el centro de la
+        figura y el borde que se encuentra siguiendo un rayo aproximadamente
+        en línea recta con ángulos que van desde 0 hasta 360 grados.
+
+        Parámetros:
+            centro_x (int): Coordenada en x del centro.
+            centro_y (int): Coordenada en y del centro.
+            hipotenusa (float): Longitud de la distancia calculada del centro
+            a los bordes.
+        
+        Regresa:
+            arregloRayos (lista): Lista que contiene las distancias
+            del centro al borde por cada uno de los 360 grados.
+        """
         arregloRayos = []
         coord_x = centro_x
         coord_y = centro_y
@@ -81,4 +152,12 @@ class AnalisisFigura:
         return arregloRayos
 
     def obtenerImagen(self):
+        """
+        Método getter para obtener el estado del arreglo
+        que representa la imagen.
+
+        Regresa:
+        imagenFigura (arreglo2D): El arreglo con la información
+        de la imagen.
+        """
         return self.imagenFigura
--- src/test/testFigura.py
+++ src/test/testFigura.py
@@ -18,7 +18,7 @@ class testFigura(unittest.TestCase):
         gi = GestorImagen(absolute_image_path)
         gi.separadorFiguras()
         figuras = gi.obtenerAlmacenador()
-        imagen1 = figuras.pop(0)
+        imagen1 = figuras.pop(1)
         arregloImagen = numpy.array(imagen1)
         af = AnalisisFigura(arregloImagen.tolist())
         return af
@@ -27,18 +27,33 @@ class testFigura(unittest.TestCase):
         af = self.crearAnalisiFigura()
         centro_x, centro_y = af.buscadorCentro()
         self.assertGreater(centro_x,0)
-        self.assertGreater(centro_x,0)
+        self.assertGreater(centro_y,0)
+        imagen = af.obtenerImagen()
+        mitad_y = int(len(imagen) / 2)
+        mitad_x = int(len(imagen[0]) / 2)
+        self.assertTrue((mitad_x <= int(centro_x)) and (int(centro_x) < mitad_x + 10))
+        self.assertTrue((mitad_y <= int(centro_y)) and (int(centro_y) < mitad_y + 10))
 
     def testCalculaHipotenusa(self):
         af = self.crearAnalisiFigura()
         hipotenusa = af.calculaHipotenusa()
         self.assertGreater(hipotenusa,0)
+        imagen = af.obtenerImagen()
+        mitad_y = int(len(imagen) / 2)
+        mitad_x = int(len(imagen[0]) / 2)
+        mitad = min(mitad_x, mitad_y)
+        self.assertLess(hipotenusa,mitad)
 
     def testDentro(self):
         af = self.crearAnalisiFigura()
         coord_x = 0
         coord_y = 0
         self.assertFalse(af.dentro(coord_x,coord_y))
+        coord_x, coord_y = af.buscadorCentro()
+        self.assertTrue(af.dentro(coord_x,coord_y))
+        h = af.calculaHipotenusa()
+        self.assertTrue(af.dentro(coord_x + (h/2),coord_y + (h/2)))
+        self.assertFalse(af.dentro(coord_x + h,coord_y + h))
 
     def testRayos(self):
         af = self.crearAnalisiFigura()
@@ -46,11 +61,8 @@ class testFigura(unittest.TestCase):
         h = af.calculaHipotenusa()
         arregloRayos = af.rayos(centro_x, centro_y, h)
         self.assertEqual(len(arregloRayos),360)
-
-    def testObtenerImagen(self):
-        af = self.crearAnalisiFigura()
-        newNumpy = numpy.asarray(af.obtenerImagen())
-        self.assertIsInstance(newNumpy,numpy.ndarray)
+        rayo = arregloRayos[0]
+        self.assertAlmostEqual(rayo, h+1)
 
 if __name__ == '__main__':
     unittest.main()
